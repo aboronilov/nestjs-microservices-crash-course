84412023f38abd1f9849d3d495e24dc15ebc5f22
Anatoly Boronilov <boronilov@gmail.com>
2024-02-26 19:05:08 +0200

    authentication

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registerDTO: RegisterDTO) {
+    await this.authService.register(registerDTO);
+  }
+
+  @Post('login')
+  async login(@Body() { email, password }: LoginDto) {
+    // await this.authService.login(registerDTO)
+    const { id } = await this.authService.validateUser(email, password);
+    return this.authService.login(id);
+  }
+}
+
+export class LoginDto {
+  email: string;
+  password: string;
+}
+
+export class RegisterDTO extends LoginDto {
+  displayName?: string;
+}
--- apps/account/src/app/auth/auth.service.ts
+++ apps/account/src/app/auth/auth.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterDTO } from './auth.controller';
+import { UserRepository } from '../user/repositories/user.repository';
+import { UserEntity } from '../user/entities/user.entity';
+import { UserRole } from '@nest-microservices-purple/interfaces';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async register({ email, password, displayName }: RegisterDTO) {
+    const userExists = await this.userRepository.findUser(email);
+    if (userExists) {
+      throw new Error(`User with email ${email} allready exists`);
+    }
+
+    const newUserEntity = await new UserEntity({
+      displayName,
+      email,
+      passwordHash: '',
+      role: UserRole.Student,
+    }).setPassword(password);
+
+    const newUser = await this.userRepository.createUser(newUserEntity);
+
+    return { email: newUser.email };
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userRepository.findUser(email);
+    if (!user) {
+      throw new Error('Wrong credentials');
+    }
+
+    const userEntity = new UserEntity(user);
+    const isValid = await userEntity.validatePassword(password);
+    if (!isValid) {
+      throw new Error('Wrong credentials');
+    }
+
+    return { id: user._id };
+  }
+
+  async login(id: string) {
+    return {
+      access_token: await this.jwtService.signAsync({ id }),
+    };
+  }
+}
