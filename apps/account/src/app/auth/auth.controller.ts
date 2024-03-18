7927ff1fc3268ceaf165bc6eabd5a44a64f543c1
Anatoly Boronilov <boronilov@gmail.com>
2024-03-18 11:17:47 +0300

    auth validate

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,19 +1,28 @@
 import { Body, Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AccountLogin, AccountRegister } from '@nest-microservices-purple/contracts';
-import { RMQRoute } from 'nestjs-rmq';
+import {
+  AccountLogin,
+  AccountRegister,
+} from '@nest-microservices-purple/contracts';
+import { RMQRoute, RMQValidate } from 'nestjs-rmq';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @RMQValidate()
   @RMQRoute(AccountRegister.topic)
-  async register(@Body() registerDTO: AccountRegister.Request): Promise<AccountRegister.Response> {
+  async register(
+    @Body() registerDTO: AccountRegister.Request
+  ): Promise<AccountRegister.Response> {
     return await this.authService.register(registerDTO);
   }
 
- @RMQRoute(AccountLogin.topic)
-  async login(@Body() { email, password }: AccountLogin.Request): Promise<AccountLogin.Response> {
+  @RMQValidate()
+  @RMQRoute(AccountLogin.topic)
+  async login(
+    @Body() { email, password }: AccountLogin.Request
+  ): Promise<AccountLogin.Response> {
     // await this.authService.login(registerDTO)
     const { id } = await this.authService.validateUser(email, password);
     return this.authService.login(id);
--- contracts/src/lib/account/account.register.ts
+++ contracts/src/lib/account/account.register.ts
@@ -1,9 +1,17 @@
+import { IsEmail, IsString, IsOptional } from 'class-validator';
+
 export namespace AccountRegister {
     export const topic = 'account.register.command'
 
     export class Request {
+        @IsEmail()
         email!: string;
+
+        @IsString()
         password!: string;
+
+        @IsString()
+        @IsOptional()
         displayName?: string;
     }
 
