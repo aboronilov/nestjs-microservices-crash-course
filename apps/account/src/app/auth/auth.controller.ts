011c0d8544015118a6f005a05c65f92ea60872c2
Anatoly Boronilov <boronilov@gmail.com>
2024-03-18 08:44:12 +0300

    contracts lib

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AccountLogin, AccountRegister } from '@nest-microservices-purple/contracts';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() registerDTO: RegisterDTO) {
-    await this.authService.register(registerDTO);
+  async register(@Body() registerDTO: AccountRegister.Request): Promise<AccountRegister.Response> {
+    return await this.authService.register(registerDTO);
   }
 
   @Post('login')
-  async login(@Body() { email, password }: LoginDto) {
+  async login(@Body() { email, password }: AccountLogin.Request): Promise<AccountLogin.Response> {
     // await this.authService.login(registerDTO)
     const { id } = await this.authService.validateUser(email, password);
     return this.authService.login(id);
